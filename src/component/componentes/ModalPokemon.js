608a932dabcabbdc40cf3b3e391f997e03b5804f
MSarpi <miguel.sarpi@gmail.com>
2023-11-03 13:57:08 -0300

    Ultimos cambios

--- src/component/componentes/ModalPokemon.js
+++ src/component/componentes/ModalPokemon.js
@@ -46,7 +46,7 @@ const Modal = ({pokemon, closeModal}) => {
                       <div className="carousel-item active" data-bs-interval="2000">
                         <img className="card-img-top img_pokemon" src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
                         <div className="">
-                          <h5 style={{ textAlign: 'center' }}>Normal</h5>
+                          {/* <h5 style={{ textAlign: 'center' }}>Normal</h5> */}
                         </div>
                       </div>
                       <div className="carousel-item" data-bs-interval="2000">
@@ -56,14 +56,14 @@ const Modal = ({pokemon, closeModal}) => {
                         </div>
                       </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                    {/* <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
                       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span className="visually-hidden">Previous</span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
                       <span className="carousel-control-next-icon" aria-hidden="true"></span>
                       <span className="visually-hidden">Next</span>
-                    </button>
+                    </button> */}
                     </div>
                     ) :pokemon.name.endsWith('sinistcha') ? (
                       <img className="card-img-top img_pokemon" src={pk_1013} alt={pokemon.name} />
